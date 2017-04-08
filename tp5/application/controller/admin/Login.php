a5b23ce841161da1b3c45642ce56a5ee94943fbc
dujinle <yingyongdujinle@163.com>
2017-04-07 21:27:04 -0700

    update for login

--- tp5/application/controller/admin/Login.php
+++ tp5/application/controller/admin/Login.php
@@ -2,6 +2,7 @@
 namespace app\controller\admin;
 use think\Db;
 use think\Controller;
+use think\View;
 class Login extends Controller
 {
     public function index()
@@ -10,10 +11,13 @@ class Login extends Controller
     }
 	public function login()
 	{
-		$name = $_POST['username'];
-		$pwd = $_POST['pass'];
-		$user = Db::name('auser')->find();
-		print_r($user);
-		return 'name:' . $name .' pwd:' . $pwd;
+		#TODO
+#		$name = $_POST['username'];
+#		$pwd = $_POST['pass'];
+#		$user = Db::name('auser')->find();
+#		print_r($user);
+#		return 'name:' . $name .' pwd:' . $pwd;
+		$view = new View();
+		return $view->fetch('admin/index/index');
 	}
 }
